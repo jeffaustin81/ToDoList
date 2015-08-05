4927ceb5446b37d64e402d58337f3083c98e204b
Jeff Austin & Christopher Calascione <student@epicodus.com>
2015-08-05 14:31:06 -0700

    fixed indentation

--- app/app.php
+++ app/app.php
@@ -1,36 +1,34 @@
 <?php
-  require_once __DIR__."/../vendor/autoload.php";
-  require_once __DIR__."/../src/Task.php";
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Task.php";
 
-  session_start();
-  if (empty($_SESSION['list_of_tasks'])) {
-    $_SESSION['list_of_tasks'] = array();
-  }
+    session_start();
+    if (empty($_SESSION['list_of_tasks'])) {
+        $_SESSION['list_of_tasks'] = array();
+    }
 
-  $app = new Silex\Application();
+    $app = new Silex\Application();
 
-  $app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/../views'
-  ));
+    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'
+    ));
 
-  $app->get("/", function() use ($app) {
+    $app->get("/", function() use ($app) {
 
-      return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
-  });
+    return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
+    });
 
-  $app->post("/tasks", function() use ($app) {
-    $task = new Task($_POST['description']);
-    $task->save();
+    $app->post("/tasks", function() use ($app) {
+        $task = new Task($_POST['description']);
+        $task->save();
 
     return $app['twig']->render('create_task.html.twig', array('newtask' => $task));
-  });
+    });
 
-  $app->post("/delete_tasks", function() use ($app) {
+    $app->post("/delete_tasks", function() use ($app) {
+        Task::deleteAll();
+        return $app['twig']->render('delete_tasks.html.twig');
+    });
 
-    Task::deleteAll();
-
-    return $app['twig']->render('delete_tasks.html.twig');
-  });
-
-  return $app;
+    return $app;
  ?>
+ 
--- src/Task.php
+++ src/Task.php
@@ -1,37 +1,37 @@
 <?php
 class Task
 {
-  private $description;
+    private $description;
 
-  function __construct($description)
-  {
-    $this->description = $description;
-  }
+    function __construct($description)
+    {
+        $this->description = $description;
+    }
 
-  function setDescription($new_description)
-  {
-    $this->description = (string) $new_description;
-  }
+    function setDescription($new_description)
+    {
+        $this->description = (string) $new_description;
+    }
 
-  function getDescription()
-  {
-    return $this->description;
-  }
+    function getDescription()
+    {
+        return $this->description;
+    }
 
-  function save()
-  {
-    array_push($_SESSION['list_of_tasks'], $this);
-  }
+    function save()
+    {
+        array_push($_SESSION['list_of_tasks'], $this);
+    }
 
-  static function getAll()
-  {
-    return $_SESSION['list_of_tasks'];
-  }
+    static function getAll()
+    {
+        return $_SESSION['list_of_tasks'];
+    }
 
-  static function deleteAll()
-  {
-    $_SESSION['list_of_tasks'] = array();
-  }
+    static function deleteAll()
+    {
+        $_SESSION['list_of_tasks'] = array();
+    }
 
 
 }
