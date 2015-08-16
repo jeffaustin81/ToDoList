88b11b7cd4f22e73d1a1910a76f501d1a123d9b7
Jeff Austin <jeffaustin81@gmail.com>
2015-08-16 13:48:27 -0700

    Added categories.twig and added additional routes for categories.twig

--- app/app.php
+++ app/app.php
@@ -3,10 +3,7 @@
     require_once __DIR__."/../src/Task.php";
     require_once __DIR__."/../src/Category.php";
 
-    session_start();
-    if (empty($_SESSION['list_of_tasks'])) {
-        $_SESSION['list_of_tasks'] = array();
-    }
+    $DB = new PDO('mysql:host=localhost:3306;dbname=to_do');
 
     $app = new Silex\Application();
 
@@ -14,19 +11,39 @@
     ));
 
     $app->get("/", function() use ($app) {
+        return $app['twig']->render('index.twig');
+    });
+    
+    $app->get("/tasks", function() use ($app) {
         return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
     });
 
+    $app->get("/categories", function() use ($app) {
+        return $app['twig']->render('categories.twig', array('categories' => Category::getAll()));
+    });
+    
     $app->post("/tasks", function() use ($app) {
         $task = new Task($_POST['description']);
         $task->save();
 
-    return $app['twig']->render('create_task.html.twig', array('newtask' => $task));
+    return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
     });
 
     $app->post("/delete_tasks", function() use ($app) {
         Task::deleteAll();
-        return $app['twig']->render('delete_tasks.html.twig');
+        return $app['twig']->render('index.twig');
+    });
+    
+    $app->post("/categories", function() use ($app) {
+        $category = new Category($_POST['name']);
+        $category->save();
+
+    return $app['twig']->render('categories.twig', array('categories' => Category::getAll()));
+    });
+
+    $app->post("/delete_categories", function() use ($app) {
+        Category::deleteAll();
+        return $app['twig']->render('index.twig');
     });
 
     return $app;
